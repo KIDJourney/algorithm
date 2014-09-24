41593b8f9819e87163d9e96a6b8ae0e9a095bbda
KIDJourney <kingdeadfish@qq.com>
2014-09-24 22:14:24 +0800

    first update

---
+++ test.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+    	reduce(lambda x,y : x^y , A)
+
+#This problem is solved by using bit manipulation   	
+#When same nums xor we will get 0
+#When a num xor 0 we will get origin num
+#It's very smart to use this way
+#I have learn a lot
