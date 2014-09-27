7ae4dfdd2b8f2c6e29d95f3488c54890b4a0fe17
KIDJourney <kingdeadfish@qq.com>
2014-09-27 14:46:52 +0800

    update

---
+++ SameTree.py
@@ -0,0 +1,13 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param p, a tree node
+    # @param q, a tree node
+    # @return a boolean
+    def isSameTree(self, p, q):
+    	
