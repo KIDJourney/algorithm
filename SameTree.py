b56d9cf7b15f90f8c00b46226cd432cab589fdc4
Lonesome George <kingdeadfish@qq.com>
2014-09-27 22:17:10 +0800

    update

--- SameTree.py
+++ SameTree.py
@@ -9,14 +9,9 @@ class Solution:
     # @param p, a tree node
     # @param q, a tree node
     # @return a boolean
-<<<<<<< HEAD
     def isSameTree(self, p, q) :
         if p is None or q is None :
             return p == q
         if p.val != q.val  :
             return False
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
-=======
-    def isSameTree(self, p, q):
-    	
->>>>>>> 7ae4dfdd2b8f2c6e29d95f3488c54890b4a0fe17
