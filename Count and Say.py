52e77d1a0a20b72fcf6bda5c2f6204ee2e4eac38
KIDJourney <kingdeadfish@qq.com>
2015-03-22 20:08:12 +0800

    update

--- Count and Say.py
+++ Count and Say.py
@@ -4,4 +4,28 @@
 # @Author: KIDJourney
 # @Email: kingdeadfish@qq.com
 # @Date:   2015-02-01
-print int("-0012a42")
+class Solution:
+    # @return a string
+    def countAndSay(self, n):
+        self.ans = "1"
+        for i in range(n-1) :
+            self.ans = self.doit()
+        return self.ans
+
+    def doit(self):
+        temp = ""
+        num = '0'
+        nnum = 0
+        lengh = len(self.ans)
+        for i in range(lengh):
+            if nnum == 0 :
+                num = self.ans[0]
+            if num != self.ans[i] :
+                temp += str(nnum) + num
+                num = self.ans[i]
+                nnum = 1
+            else :
+                nnum += 1
+            if i == lengh -1 :
+                temp += str(nnum) + num
+        return temp
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Position: E:\Program\LeetCoder_Python\test.py
-# @Author: KIDJourney
-# @Email: kingdeadfish@qq.com
-# @Date:   2014-09-25
-print int("+-10")
+# @Author: kidjourney
+# @Date:   2015-02-20 23:54:20
+# @Last Modified by:   kidjourney
+# @Last Modified time: 2015-02-21 11:15:21
+string = 'sb'
+string += 'sbsb'
+print string
