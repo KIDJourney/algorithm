bc34dcfe08294d38f17875c30cd778d01ad936c1
KIDJourney <kingdeadfish@qq.com>
2015-04-30 20:04:30 +0800

    update

--- algorithm/test.py
+++ algorithm/test.py
@@ -3,10 +3,10 @@
 # @Author: kidjourney
 # @Date:   2015-04-29 19:48:35
 # @Last Modified by:   kidjourney
-# @Last Modified time: 2015-04-29 21:20:54
-a = 10 
-while a > 0 :
-    print a 
-    a -= 1
-else :
-    print "HAHA"
+# @Last Modified time: 2015-04-29 21:29:06
+A = "^__^" 
+B = "T.T"
+for i in range(10) :
+    C = A + B
+    A , B = B , C
+    print C
