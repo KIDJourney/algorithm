7dec1f80e916a0d300539c9512ed8778790d2b80
KIDJourney <kingdeadfish@qq.com>
2015-06-13 12:33:54 +0800

    update

--- algorithm/Add Two Numbers.py
+++ algorithm/Add Two Numbers.py
@@ -1,8 +1,8 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     # @param {ListNode} l1
@@ -16,26 +16,21 @@ class Solution:
             pos *= 10
             l1 = l1.next
             l2 = l2.next
+        while l1 :
+            ans += (l1.val) * pos
+            pos *= 10
+            l1 = l1.next
+        while l2 :
+            ans += (l2.val) * pos
+            pos *= 10
+            l2 = l2.next
         print (ans)
         head = ListNode(0)
         s = head
+        if not ans :
+            return head
         while ans :
             s.next = ListNode(ans%10)
             s = s.next
             ans = int(ans/10)
         return head.next
-
-
-def showList(l1) :
-    if l1.next :
-        showList(l1.next)
-    print(l1.val)
-
-l1 = ListNode(1)
-l1.next = ListNode(2)
-
-l2 = ListNode(2)
-l2.next = ListNode(5)
-
-job = Solution()
-showList(job.addTwoNumbers(l1,l2))
