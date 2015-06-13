73a4ecfb1b33d5f5441f8348eb6d3cc41bf113fd
KIDJourney <kingdeadfish@qq.com>
2015-06-13 12:24:53 +0800

    update

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,8 +1,2 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: kidjourney
-# @Date:   2015-05-20 23:53:24
-# @Last Modified by:   kidjourney
-# @Last Modified time: 2015-05-21 00:08:41
-a = [1,2,3,4]
-print(max(a)) if 10 < 15
+mydir = {1:1,2:2}
+print(mydir.setdefault(1,1))
