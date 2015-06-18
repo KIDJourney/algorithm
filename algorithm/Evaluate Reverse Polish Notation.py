e89ba22ac3601412f6adb199f6750820e000c22e
KIDJourney <kingdeadfish@qq.com>
2015-06-19 00:35:44 +0800

    update

--- algorithm/Evaluate Reverse Polish Notation.py
+++ algorithm/Evaluate Reverse Polish Notation.py
@@ -3,22 +3,19 @@
 # @Author: kidjourney
 # @Date:   2015-06-17 22:45:12
 # @Last Modified by:   kidjourney
-# @Last Modified time: 2015-06-17 23:02:29
+# @Last Modified time: 2015-06-19 00:35:27
 class Solution:
     # @param {string[]} tokens
     # @return {integer}
     def evalRPN(self, tokens):
         stack = []
-        symbol = ['+' , '-' , '*' , '/']
+        symbolmap = {'+': lambda y,x : x+y ,
+                     '-': lambda y,x : x-y ,
+                     '*': lambda y,x : x*y ,
+                     '/': lambda y,x : int(operator.truediv(x,y))}
         for i in tokens :
-            print(stack)
-            if i in symbol :
-                b = stack.pop()
-                a = stack.pop()
-                stack.append(int(eval("{0} {1} {2}".format(a,i,b))))
+            if i in symbolmap :
+                stack.append(symbolmap[i]((stack.pop()) , stack.pop()))
             else :
                 stack.append(int(i))
-        return int(stack[0])
-
-job = Solution()
-print(job.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+        return stack[0]
--- algorithm/test.py
+++ algorithm/test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: kidjourney
-# @Date:   2015-06-15 23:35:16
+# @Date:   2015-06-17 22:50:53
 # @Last Modified by:   kidjourney
-# @Last Modified time: 2015-06-17 22:50:51
-mylist = []
-mylist.append(10)
-mylist.append(1)
-mylist.pop()
-a = "{0} {1} {2}".format(12,'+',12)
-print (a)
-print(mylist)
+# @Last Modified time: 2015-06-19 00:20:28
+def show(a,b) :
+    print(a,b)
+
+mystack = [1,2,3,4]
+show(mystack.pop() , (mystack.pop()))
+mystack = [1,2,3,4]
+show(mystack.pop() , mystack.pop())
