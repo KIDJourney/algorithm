b942e1c4e800915553bf788bde5d1dff98f05ca6
KIDJourney <kingdeadfish@qq.com>
2015-06-19 23:31:08 +0800

    update

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: kidjourney
-# @Date:   2015-06-17 22:50:53
+# @Date:   2015-06-19 00:20:29
 # @Last Modified by:   kidjourney
-# @Last Modified time: 2015-06-19 00:20:28
-def show(a,b) :
-    print(a,b)
-
-mystack = [1,2,3,4]
-show(mystack.pop() , (mystack.pop()))
-mystack = [1,2,3,4]
-show(mystack.pop() , mystack.pop())
+# @Last Modified time: 2015-06-19 11:19:01
+grid = [[i for i in range(11)] for j in range(10)]
+print(len(grid[1]))
