477ea00fb01f1497734328c70325bbd7d33df169
KIDJourney <kingdeadfish@qq.com>
2015-07-10 10:08:47 -0400

    update

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: kidjourney
-# @Date:   2015-06-19 00:20:29
-# @Last Modified by:   kidjourney
-# @Last Modified time: 2015-06-19 11:19:01
-grid = [[i for i in range(11)] for j in range(10)]
-print(len(grid[1]))
+a = 1
+b = 0
+a , b = a+1 , a
+print(b)
