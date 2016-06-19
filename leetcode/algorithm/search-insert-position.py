455f9f8947ec3de44fb94d088a372e2317ba9d07
KIDJourney <kingdeadfish@qq.com>
2016-06-19 22:33:19 +0800

    move

--- leetcode/algorithm/search-insert-position.py
+++ leetcode/algorithm/search-insert-position.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Position: E:\Program\algorithm\search-insert-position.py
+# @Author: KIDJourney
+# @Email: kingdeadfish@qq.com
+# @Date:   2016-03-05
 class Solution(object):
     def searchInsert(self, nums, target):
         """
@@ -5,4 +11,12 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
+        e = len(nums)-1
+        s = 0
+        while s <= e :
+            mid = (s + e) // 2
+            if target > nums[mid] : 
+                s = mid + 1
+            else :
+                e = mid - 1
+        return s
