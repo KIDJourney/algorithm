a3aa15cb87a486d88dcefa15af378ef497227480
KIDJourney <kingdeadfish@qq.com>
2018-12-11 02:00:25 +0800

    update

--- leetcode/algorithm/maximum-sum-circular-subarray.py
+++ leetcode/algorithm/maximum-sum-circular-subarray.py
@@ -4,4 +4,31 @@ class Solution(object):
         :type A: List[int]
         :rtype: int
         """
-        
+        ret = 0
+        length = len(A)
+
+        first_max = 0
+
+        left, right = 0, 0
+
+        max_sum = 0
+        tsum = 0
+        while right < length:
+            if A[right] + tsum > 0:
+                max_sum = max(A[right] + tsum, max_sum)
+                if left == 0:
+                    first_max = max_sum
+            else:
+                left = right
+                tsum = 0
+
+            right += 1
+
+        if left == 0:
+            return max_sum
+        else:
+            return first_max + max_sum
+
+
+s = Solution()
+print(s.maxSubarraySumCircular([1, -2, 3, -2]))
---
+++ leetcode/algorithm/sliding-puzle.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def slidingPuzzle(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: int
+        """
+        if not board:
+            return -1
