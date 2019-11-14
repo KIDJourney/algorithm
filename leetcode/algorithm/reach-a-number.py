09d3c7832bb6a3f1fe16d248eea51b546b3e1c84
KIDJourney <kingdeadfish@qq.com>
2019-11-14 17:54:55 +0800

    update

---
+++ leetcode/algorithm/reach-a-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reachNumber(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        ts = 0
+        for i in range(1, 100000)
